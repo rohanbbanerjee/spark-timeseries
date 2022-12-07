9c890751bbe5813763c6ff2547dbe02dcdbbe28e
Yash Varun <yash.v.varun@oracle.com>
2022-12-07 11:07:43 +0530

    Fixing 'constructor invokes overidable function'

--- src/test/scala/com/cloudera/sparkts/models/ARIMAXSuite.scala
+++ src/test/scala/com/cloudera/sparkts/models/ARIMAXSuite.scala
@@ -20,7 +20,7 @@ import org.apache.spark.mllib.linalg.DenseVector
 import org.scalatest.FunSuite
 import org.scalatest.Matchers._
 
-private class ARIMAXSuite extends FunSuite {
+final class ARIMAXSuite extends FunSuite {
   // Data from http://www.robjhyndman.com/data/ - command to use this data available on website
   // robjhyndman.com/talks/RevolutionR/exercises1.pdf
 
